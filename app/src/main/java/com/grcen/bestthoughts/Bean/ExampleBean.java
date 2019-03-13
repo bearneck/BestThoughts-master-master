3f50e6e048c05cdb61f9bcabc90b8bb182ac15ee
bearneck <haogaoha@126.com>
2019-03-14 01:27:24 +0800

    detail还差网络请求和评论功能

---
+++ app/src/main/java/com/grcen/bestthoughts/Bean/ExampleBean.java
@@ -0,0 +1,13 @@
+package com.grcen.bestthoughts.Bean;
+
+public class ExampleBean {
+    private int viewType;
+
+    public int getViewType() {
+        return viewType;
+    }
+
+    public void setViewType(int viewType) {
+        this.viewType = viewType;
+    }
+}
